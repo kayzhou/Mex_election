603fc9a208e58e5bbd108c5fafa0a693d232c5f0
Kay <zhenkun91@outlook.com>
2020-08-17 19:58:43 +0800

    u

--- myclf.py
+++ myclf.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectFromModel
-from sklearn.grid_search import GridSearchCV
+# from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score, train_test_split
@@ -78,16 +78,16 @@ def svm_linear_classifier(train_x, train_y):
 
 
 # SVM Classifier using cross validation
-def svm_cross_validation(train_x, train_y):
+# def svm_cross_validation(train_x, train_y):
 
-    model = SVC(kernel='rbf', probability=True)
-    param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10,
-                        100, 1000], 'gamma': [0.001, 0.0001]}
-    grid_search = GridSearchCV(model, param_grid, n_jobs=1, verbose=1)
-    grid_search.fit(train_x, train_y)
-    best_parameters = grid_search.best_estimator_.get_params()
-    for para, val in list(best_parameters.items()):
-        print(para, val)
-    model = SVC(kernel='rbf', C=best_parameters['C'],
-                gamma=best_parameters['gamma'], probability=True)
-    model.fit(train_x, train_y)
+#     model = SVC(kernel='rbf', probability=True)
+#     param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10,
+#                         100, 1000], 'gamma': [0.001, 0.0001]}
+#     grid_search = GridSearchCV(model, param_grid, n_jobs=1, verbose=1)
+#     grid_search.fit(train_x, train_y)
+#     best_parameters = grid_search.best_estimator_.get_params()
+#     for para, val in list(best_parameters.items()):
+#         print(para, val)
+#     model = SVC(kernel='rbf', C=best_parameters['C'],
+#                 gamma=best_parameters['gamma'], probability=True)
+#     model.fit(train_x, train_y)
