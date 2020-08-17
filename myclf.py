f7deb089d806f81fe8458a98b19eac8b013ee77b
Kay <zhenkun91@outlook.com>
2020-08-17 19:54:59 +0800

    u

--- myclf.py
+++ myclf.py
@@ -3,21 +3,19 @@
 # Date: 2019-03-30 19:19:22
 
 import joblib
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
-from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
 
 # Multinomial Naive Bayes Classifier
 def naive_bayes_classifier(train_x, train_y):
-    from sklearn.naive_bayes import MultinomialNB
     model = MultinomialNB(alpha=0.01)
     model.fit(train_x, train_y)
     return model
@@ -25,7 +23,6 @@ def naive_bayes_classifier(train_x, train_y):
 
 # KNN Classifier
 def knn_classifier(train_x, train_y):
-    from sklearn.neighbors import KNeighborsClassifier
     model = KNeighborsClassifier()
     model.fit(train_x, train_y)
     return model
@@ -33,7 +30,6 @@ def knn_classifier(train_x, train_y):
 
 # Logistic Regression Classifier
 def logistic_regression_classifier(train_x, train_y):
-    from sklearn.linear_model import LogisticRegression
     model = LogisticRegression(penalty='l2')
     model.fit(train_x, train_y)
     return model
@@ -41,7 +37,6 @@ def logistic_regression_classifier(train_x, train_y):
 
 # Random Forest Classifier
 def random_forest_classifier(train_x, train_y):
-    from sklearn.ensemble import RandomForestClassifier
     model = RandomForestClassifier(n_estimators=8)
     model.fit(train_x, train_y)
     return model
@@ -49,7 +44,6 @@ def random_forest_classifier(train_x, train_y):
 
 # Decision Tree Classifier
 def decision_tree_classifier(train_x, train_y):
-    from sklearn import tree
     model = tree.DecisionTreeClassifier()
     model.fit(train_x, train_y)
     return model
@@ -57,7 +51,6 @@ def decision_tree_classifier(train_x, train_y):
 
 # GBDT(Gradient Boosting Decision Tree) Classifier
 def gradient_boosting_classifier(train_x, train_y):
-    from sklearn.ensemble import GradientBoostingClassifier
     model = GradientBoostingClassifier(n_estimators=200)
     model.fit(train_x, train_y)
     return model
@@ -65,7 +58,6 @@ def gradient_boosting_classifier(train_x, train_y):
 
 # SVM Classifier
 def svm_classifier(train_x, train_y):
-    from sklearn.svm import SVC
     model = SVC(kernel='rbf', probability=True)
     model.fit(train_x, train_y)
     return model
@@ -80,8 +72,7 @@ def svm_linear_classifier(train_x, train_y):
 
 # SVM Classifier using cross validation
 def svm_cross_validation(train_x, train_y):
-    from sklearn.grid_search import GridSearchCV
-    from sklearn.svm import SVC
+
     model = SVC(kernel='rbf', probability=True)
     param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10,
                         100, 1000], 'gamma': [0.001, 0.0001]}
