f49c689fcf30be893b2f2578b6e3080d4f11d685
Kay <zhenkun91@outlook.com>
2020-08-24 00:03:15 +0800

    u

--- daily_run.py
+++ daily_run.py
@@ -6,15 +6,16 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/23 23:43:19 by Zhenkun           #+#    #+#              #
-#    Updated: 2020/08/24 00:00:25 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/08/24 00:03:11 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from MySQL_handler import *
 from my_weapon import *
 
-start = pendulum.date(2020, 8, 1)
-end = pendulum.date(2020, 8, 21)
+if __name__ == "__main__":
+    start = pendulum.datetime(2020, 8, 1, tz="UTC")
+    end = pendulum.datetime(2020, 8, 21, tz="UTC")
 
-for dt in pendulum.Period(start, end):
-    upsert_all_query_freq(dt)
+    for dt in pendulum.Period(start, end):
+        upsert_all_query_freq(dt)
