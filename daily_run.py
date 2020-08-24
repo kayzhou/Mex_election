376ac90d78c0cf734552e9b9e8da3814ca56651a
Kay <zhenkun91@outlook.com>
2020-08-24 22:24:34 +0800

    u

--- daily_run.py
+++ daily_run.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/23 23:43:19 by Zhenkun           #+#    #+#              #
-#    Updated: 2020/08/24 22:20:23 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/08/24 22:24:03 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,5 +20,6 @@ if __name__ == "__main__":
     end = pendulum.datetime(2020, 8, 21, tz="UTC")
 
     for dt in pendulum.Period(start, end):
+        print(dt)
         # insert_all_query_freq(dt)
         tweets_to_db(dt, dt.add(days=1))
