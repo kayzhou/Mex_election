5c2d682ed7db6d0c5a19c0ae22bc5024f14a05d1
Kay <zhenkun91@outlook.com>
2020-08-31 21:26:09 +0800

    u

--- daily_run.py
+++ daily_run.py
@@ -6,7 +6,7 @@
 #    By: Zhenkun <zhenkun91@outlook.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/23 23:43:19 by Zhenkun           #+#    #+#              #
-#    Updated: 2020/08/24 22:24:03 by Zhenkun          ###   ########.fr        #
+#    Updated: 2020/08/31 21:26:05 by Zhenkun          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,9 +17,10 @@ from my_weapon import *
 if __name__ == "__main__":
     # start = pendulum.datetime(2020, 8, 1, tz="UTC")
     start = pendulum.datetime(2020, 8, 20, tz="UTC")
-    end = pendulum.datetime(2020, 8, 21, tz="UTC")
+    end = pendulum.datetime(2020, 8, 30, tz="UTC")
 
     for dt in pendulum.Period(start, end):
         print(dt)
-        # insert_all_query_freq(dt)
-        tweets_to_db(dt, dt.add(days=1))
+        insert_all_query_freq(dt)
+    
+    # tweets_to_db(dt, dt.add(days=1))
